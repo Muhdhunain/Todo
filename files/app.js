a4c0648a2d4845b2154c19d3c0765a6a918abd1c
Muhdhunain <hunainibrahim33@gmail.com>
2021-12-30 01:27:12 +0500

    v.1

--- files/app.js
+++ files/app.js
@@ -3,23 +3,27 @@ var todoUli = document.getElementById('todoUli')
 
 function addTodo() {
     var tdInp = todoInp.value
+    
+    //creation of li
     var todoLi = document.createElement('li')
     var txt = document.createTextNode(tdInp)
     todoLi.appendChild(txt)
-
+    
     todoInp.value = "";
-
+    
     //creation of delete button
     var liDel = document.createElement('button')
     var txtDel = document.createTextNode('Delete')
     liDel.setAttribute('onclick', 'del(this)')
+    liDel.setAttribute('class', 'btn1')
     liDel.appendChild(txtDel)
     todoLi.appendChild(liDel)
-
+    
     //creation of edit button
     var liEdi = document.createElement('button')
     var txtEdi = document.createTextNode('Edit')
     liEdi.setAttribute('onclick', 'edi(this)')
+    liEdi.setAttribute('class', 'btn1')
     liEdi.appendChild(txtEdi)
     todoLi.appendChild(liEdi)
 
