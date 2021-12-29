e195a2760990768a6b36ca704a21c183631a010e
Muhdhunain <hunainibrahim33@gmail.com>
2021-12-30 00:25:23 +0500

    v.1 incomp

---
+++ files/app.js
@@ -0,0 +1,39 @@
+var todoInp = document.getElementById('todoInp')
+var todoUli = document.getElementById('todoUli')
+
+function addTodo() {
+    var tdInp = todoInp.value
+    var todoLi = document.createElement('li')
+    var txt = document.createTextNode(tdInp)
+    todoLi.appendChild(txt)
+
+    todoInp.value = "";
+
+    //creation of delete button
+    var liDel = document.createElement('button')
+    var txtDel = document.createTextNode('Delete')
+    liDel.setAttribute('onclick', 'del(this)')
+    liDel.appendChild(txtDel)
+    todoLi.appendChild(liDel)
+
+    //creation of edit button
+    var liEdi = document.createElement('button')
+    var txtEdi = document.createTextNode('Edit')
+    liEdi.setAttribute('onclick', 'edi(this)')
+    liEdi.appendChild(txtEdi)
+    todoLi.appendChild(liEdi)
+
+
+    todoUli.appendChild(todoLi)
+}
+
+function del(dele) {
+    dele.parentNode.remove()
+}
+
+function edi(edit) {
+    edit.parentNode.firstChild.nodeValue = prompt('Enter Your New Value');
+}
+function delTodo() {
+    todoUli.innerHTML = "";
+}
